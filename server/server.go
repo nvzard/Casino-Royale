fa881dd68041708f2e8dd5abb8fad5f9375f2d68
Nitanshu Vashistha <nitanshu.vzard@gmail.com>
2022-06-05 16:28:28 +0530

    monitoring: Add prometheus and instrumentation metrics
    Signed-off-by: Nitanshu Vashistha <nitanshu.vzard@gmail.com>

--- server/server.go
+++ server/server.go
@@ -4,10 +4,12 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/Depado/ginprom"
 	ginzap "github.com/gin-contrib/zap"
 	"github.com/gin-gonic/gin"
 	"github.com/nvzard/casino-royale/controller"
 	"github.com/nvzard/casino-royale/utils"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"go.uber.org/zap"
 )
 
@@ -21,6 +23,14 @@ func init() {
 func SetupApiServer() *gin.Engine {
 	r := gin.New()
 
+	// Gin-prometheus
+	p := ginprom.New(
+		ginprom.Engine(r),
+		ginprom.Subsystem("gin"),
+		ginprom.Path("/metrics"),
+	)
+	r.Use(p.Instrument())
+
 	// Middleware
 	r.Use(gin.Recovery())
 	r.Use(ginzap.Ginzap(utils.Logger(), time.RFC3339, true))
@@ -29,6 +39,9 @@ func SetupApiServer() *gin.Engine {
 	// Root Routes
 	r.GET("/", root)
 	r.GET("/health", healthcheck)
+
+	// Metrics for prometheus
+	r.Any("/metrics/*query", gin.WrapH(promhttp.Handler()))
 
 	// Deck API routes
 	r.POST("/deck", controller.CreateDeck)
